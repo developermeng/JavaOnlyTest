4186db10eebe2f5e6fe652cc0d65d74c6ca2ec1a
meng_PC <1601973251@qq.com>
2018-11-30 16:52:45 +0800

    2018-9-11 commit

--- EasyTest/src/OnlyTest/Test.java
+++ EasyTest/src/OnlyTest/Test.java
@@ -138,6 +138,53 @@ class MyStack1{
 
 }
 
+class MyStack2{
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+    public MyStack2(){
+        stackData = new Stack<Integer>();
+        stackMin = new Stack<Integer>();
+    }
+
+    public Stack<Integer> getStackData() {
+        return stackData;
+    }
+
+    public Stack<Integer> getStackMin() {
+        return stackMin;
+    }
+
+    public int getMin(){
+        if(this.stackMin.empty()){
+            throw new RuntimeException("stackMin is empty");
+        }else{
+            return this.stackMin.peek();
+        }
+    }
+
+    public void push(int num){
+        if(this.stackMin.empty()){
+            this.stackMin.push(num);
+        }else if (num < this.getMin()){
+            this.stackMin.push(num);
+        }else{
+            int newMin = this.stackMin.peek();
+            this.stackMin.push(newMin);
+        }
+        this.stackData.push(num);
+    }
+
+    public void pop(){
+        if(this.stackData.empty()){
+            throw new RuntimeException("stackData is empty");
+        }
+        this.stackData.pop();
+        this.stackMin.pop();
+    }
+
+
+}
+
 
 public class Test {
     public static void main(String []args) throws EmptyStackException, Myexception {
@@ -150,39 +197,54 @@ public class Test {
         //Class practice
         //Father1 F1 = new Father1();
 
-        StackTest t = new StackTest();
-        t.stacktest();
-        int temp = 0;
+//        StackTest t = new StackTest();
+//        t.stacktest();
+//        int temp = 0;
+//
+//        System.out.println("\n\nSmartStack Show\n");
+//
+//        MyStack1 SmartSt = new MyStack1();
+//
+//        SmartSt.push(11);
+//        temp = SmartSt.getmin();
+//        System.out.println("min of SmartStack:" + temp);
+//        SmartSt.push(33);
+//        SmartSt.push(21);
+//        SmartSt.push(2);
+//        SmartSt.push(13);
+//        SmartSt.push(0);
+//
+//        System.out.println("SmartStack : " + SmartSt.getStackData());
+//        temp = SmartSt.getmin();
+//        System.out.println("min of SmartStack:" + temp);
+//
+//        SmartSt.pop();
+//
+//        System.out.println("SmartStack : " + SmartSt.getStackData());
+//        temp = SmartSt.getmin();
+//        System.out.println("min of SmartStack:" + temp);
+//
+//        SmartSt.pop();
+//        SmartSt.pop();
+//
+//        System.out.println("SmartStack : " + SmartSt.getStackData());
+//        temp = SmartSt.getmin();
+//        System.out.println("min of SmartStack:" + temp);
+        MyStack2 smartSt2 = new MyStack2();
+        smartSt2.push(11);
+        smartSt2.push(1);
+        smartSt2.push(2);
+        smartSt2.push(25);
+        smartSt2.push(1);
+        System.out.println("StackData: " + smartSt2.getStackData());
+        System.out.println("MinData: " + smartSt2.getStackMin());
+        System.out.println("MinNum: " + smartSt2.getMin());
+        smartSt2.push(-1);
+        smartSt2.push(0);
+        System.out.println("StackData: " + smartSt2.getStackData());
+        System.out.println("MinData: " + smartSt2.getStackMin());
+        System.out.println("MinNum: " + smartSt2.getMin());
 
-        System.out.println("\n\nSmartStack Show\n");
-
-        MyStack1 SmartSt = new MyStack1();
-
-        SmartSt.push(11);
-        temp = SmartSt.getmin();
-        System.out.println("min of SmartStack:" + temp);
-        SmartSt.push(33);
-        SmartSt.push(21);
-        SmartSt.push(2);
-        SmartSt.push(13);
-        SmartSt.push(0);
-
-        System.out.println("SmartStack : " + SmartSt.getStackData());
-        temp = SmartSt.getmin();
-        System.out.println("min of SmartStack:" + temp);
-
-        SmartSt.pop();
-
-        System.out.println("SmartStack : " + SmartSt.getStackData());
-        temp = SmartSt.getmin();
-        System.out.println("min of SmartStack:" + temp);
-
-        SmartSt.pop();
-        SmartSt.pop();
-
-        System.out.println("SmartStack : " + SmartSt.getStackData());
-        temp = SmartSt.getmin();
-        System.out.println("min of SmartStack:" + temp);
 
     }
 }
